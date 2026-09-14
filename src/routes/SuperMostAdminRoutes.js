5819664baa83aaa61f3baf58a223d3376aabb156
Shivam Sharma <rudrashivam0p@gmail.com>
2026-09-14 13:13:30 +0530

    Fix the Build issue

--- src/routes/SuperMostAdminRoutes.js
+++ src/routes/SuperMostAdminRoutes.js
@@ -19,7 +19,7 @@ const HrmsElectricityReport = Loadable(lazy(() => import('views/SuperMostAdmin/H
 const HrmsPreProcessingReport = Loadable(lazy(() => import('views/SuperMostAdmin/HRMS/Reports/PreProcessingReport')));
 const HrmsSaralReport = Loadable(lazy(() => import('views/SuperMostAdmin/HRMS/Reports/SaralReport')));
 const HrmsAttendanceExceptionReport = Loadable(lazy(() => import('views/SuperMostAdmin/HRMS/Reports/AttendanceExceptionReport')));
-const HrmsRegularisations = Loadable(lazy(() => import('@/views/SuperMostAdmin/HRMS/Regularisations/Regularisations')));
+const HrmsRegularisations = Loadable(lazy(() => import('views/SuperMostAdmin/HRMS/Regularisations/Regularisations')));
 const HrmsAnnouncements = Loadable(lazy(() => import('views/SuperMostAdmin/HRMS/Announcements')));
 const HrmsCreateAnnouncement = Loadable(lazy(() => import('views/SuperMostAdmin/HRMS/Announcements/components/CreateAnnouncement')));
 const HostelDashboard = Loadable(lazy(() => import('views/SuperMostAdmin/HostelAdministration/Dashboard')));
