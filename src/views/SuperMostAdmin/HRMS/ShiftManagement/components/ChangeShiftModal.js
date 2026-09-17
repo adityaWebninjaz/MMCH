8c8c70de67a29603096c093fc2b9b91a81f6cfa3
HemantKumar110501 <hemantkumar110501@gmail.com>
2026-09-17 13:06:21 +0530

    refactor: update getShiftDetails import path and remove commented-out import

--- src/views/SuperMostAdmin/HRMS/ShiftManagement/components/ChangeShiftModal.js
+++ src/views/SuperMostAdmin/HRMS/ShiftManagement/components/ChangeShiftModal.js
@@ -17,7 +17,7 @@ import {
   Close as CloseIcon,
   Search as SearchIcon
 } from '@mui/icons-material';
-import { getShiftDetails } from '@/views/SuperMostAdmin/HRMS/ShiftManagement/Services/shiftDetailServices';
+import { getShiftDetails } from 'views/SuperMostAdmin/HRMS/ShiftManagement/Services/shiftDetailServices';
 
 
 const DEFAULT_EMPTY_ARRAY = [];
